806ffb3ee96d569edd52bb058e857b85f4e7393b
luasenvy <luas.envy@gmail.com>
2024-01-03 10:32:49 +0900

    fix: clean code, change command name

--- src/extension.ts
+++ src/extension.ts
@@ -3,42 +3,32 @@
 import * as vscode from 'vscode';
 import { exec } from 'child_process';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "nodeline" is now active!');
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  const run = vscode.commands.registerCommand('nodeline.run', () => {
-
-    const editor = vscode.window.activeTextEditor;
-    if (editor) {
-      const document = editor.document;
-      vscode.window.activeTextEditor?.edit(edit => {
-        editor.selections.forEach(selection => {
-          if (selection && !selection?.isEmpty) {
-            const code = document.getText(selection);
-            try {
-              edit.replace(selection, `${code} = ${eval(code)}`);
-            } catch(err) {
-              if (err instanceof Error) {
-                edit.replace(selection, `${code} = ${err.name}: ${err.message}`);
-              }
-              console.error(err);
+const handleRun = () => {
+  const editor = vscode.window.activeTextEditor;
+  if (editor) {
+    const document = editor.document;
+    vscode.window.activeTextEditor?.edit(edit => {
+      editor.selections.forEach(selection => {
+        if (selection && !selection?.isEmpty) {
+          const code = document.getText(selection);
+          try {
+            edit.replace(selection, `${code} = ${eval(code)}`);
+          } catch(err) {
+            if (err instanceof Error) {
+              edit.replace(selection, `${code} = ${err.name}: ${err.message}`);
             }
+            console.error(err);
           }
-        });
+        }
       });
-      }
-  });
+    });
+  };
+};
 
+export const activate = (context: vscode.ExtensionContext) => {
+  const run = vscode.commands.registerCommand('nodeline.run', handleRun);
+  
   context.subscriptions.push(run);
-}
+};
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export const deactivate = () => {};
