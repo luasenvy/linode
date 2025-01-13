9dce2f2b57d860d27bc11c2f1e2ba2dbf91363f5
luasenvy <luas.envy@gmail.com>
2025-01-13 14:56:12 +0900

    refactor: eslint, ts, syntax

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,17 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 const handleRun = () => {
   const editor = vscode.window.activeTextEditor;
   if (editor) {
     const document = editor.document;
-    vscode.window.activeTextEditor?.edit(edit => {
-      editor.selections.forEach(selection => {
+    vscode.window.activeTextEditor?.edit((edit) => {
+      editor.selections.forEach((selection) => {
         if (selection && !selection?.isEmpty) {
           const text = document.getText(selection);
-          const code = text.replace(/=(\s+)?$/, '');
+          const code = text.replace(/=(\s+)?$/, "");
           try {
-            edit.replace(selection, text.replace(/([\s=]+)?$/, () => ' = ').concat(eval(code)));
-          } catch(err) {
+            edit.replace(selection, text.replace(/([\s=]+)?$/, () => " = ").concat(eval(code)));
+          } catch (err) {
             if (err instanceof Error) {
               edit.replace(selection, `${code} = ${err.name}: ${err.message}`);
             }
@@ -20,11 +20,11 @@ const handleRun = () => {
         }
       });
     });
-  };
+  }
 };
 
 export const activate = (context: vscode.ExtensionContext) => {
-  const run = vscode.commands.registerCommand('nodeline.run', handleRun);
+  const run = vscode.commands.registerCommand("nodeline.run", handleRun);
 
   context.subscriptions.push(run);
 };
